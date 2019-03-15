65adf20783a31c211457f5d252c6f69cbea057c3
Maria Nord-Varhaug <44210425+maria-nord-varhaug@users.noreply.github.com>
2019-03-15 13:27:49 +0100

    create workoutmachine class

---
+++ src/main/java/WorkoutMachine.java
@@ -0,0 +1,10 @@
+public class WorkoutMachine {
+    private String name;
+    private String mDescription;
+
+    public WorkoutMachine(String name, String mDescription) {
+        this.name = name;
+        this.mDescription = mDescription;
+    }
+
+}
