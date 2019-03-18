58a735e3ab51ae1ddfc9204d7c07e9b58e6ad475
Maria Nord-Varhaug <44210425+maria-nord-varhaug@users.noreply.github.com>
2019-03-18 13:10:21 +0100

    add to workoutctrl

--- src/main/java/WorkoutCtrl.java
+++ src/main/java/WorkoutCtrl.java
@@ -1,3 +1,23 @@
-public class WorkoutCtrl {
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class WorkoutCtrl extends DBConn {
+    private Scanner scanner;
+    //private Workout workout;
+
+    public WorkoutCtrl() {
+        connect();
+        try {
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            System.out.println("db error during setAuoCommit of LagAvtaleCtrl=" + e);
+            return;
+        }
+        this.scanner = new Scanner(System.in);
+    }
+
+    private void createWorkout() {
+
+    }
 
 }
