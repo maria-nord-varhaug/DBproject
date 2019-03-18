be718f9238782e2d0c1baa4f03884e378c51443a
Maria Nord-Varhaug <44210425+maria-nord-varhaug@users.noreply.github.com>
2019-03-18 14:35:37 +0100

    add more functionality to workoutctrl

--- src/main/java/WorkoutCtrl.java
+++ src/main/java/WorkoutCtrl.java
@@ -23,14 +23,42 @@ public class WorkoutCtrl extends DBConn {
         //workout = new Workout(workoutDate, workoutTime, duration, personalshape, note, exercises);
     }
 
+    public static void main(String[] args) {
+        WorkoutCtrl wc = new WorkoutCtrl();
+        wc.addWorkout();
+
+    }
+
     public void addWorkout() {
         Date date;
         Time time;
         int duration;
         int personalshape;
         String note;
-        System.out.println("Okay, so you just did a workout. What date ");
+        System.out.println("Okay, what date did you perform the workout you want to log? Write it in format yyyy-MM-dd");
+        System.out.println("Example: 2019-02-01");
+        date = java.sql.Date.valueOf(scanner.nextLine());
+        System.out.println("What time did you workout? On the form hh:mm");
+        time = java.sql.Time.valueOf(scanner.nextLine() + ":00");
+        System.out.println("Now, how long did your workout last (in minutes)? ");
+        duration = scanner.nextInt();
+        System.out.println("On a scale from 1 to 10, how was your shape during the workout? ");
+        personalshape = scanner.nextInt();
+        System.out.println("Do you have any additional comments you want to add?");
+        note = scanner.nextLine();
+        //List<Exercise> exerciseList = new ArrayList<Exercise>();
+        //createWorkout(date, time, duration, personalshape, note, exerciseList);
 
+        String querystring = "Did you do any exercises you want to log? If you do write 'Y'";
+        System.out.println(querystring);
+        //System.out.println(date.toString()+time.toString()+duration+personalshape+note); //test
+        String answer = scanner.nextLine();
 
+        while (answer.equals("Y")) {
+            //TODO: create exercise
+            //
+            System.out.println(querystring);
+            answer = scanner.nextLine();
+        }
     }
 }
